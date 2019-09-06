e30d8a14c8d3450b2916ba5451c1c2b7099eb80c
Matt Bretl <mpbretl@gmail.com>
2019-09-06 12:38:36 -0500

    add test for `@omit manyToMany` on constraint

--- __tests__/p-schema.sql
+++ __tests__/p-schema.sql
@@ -40,3 +40,12 @@ create table p.baz (
 );
 
 comment on constraint baz_bar_id_fkey on p.baz is E'@omit';
+
+create table p.qux (
+  foo_id int constraint qux_foo_id_fkey references p.foo (id),
+  bar_id int constraint qux_bar_id_fkey references p.bar (id),
+  primary key (foo_id, bar_id)
+);
+
+comment on table p.qux is E'@omit all';
+comment on constraint qux_bar_id_fkey on p.qux is E'@omit manyToMany';
