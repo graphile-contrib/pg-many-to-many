ef44d257f9e5520077675410def312ddb7a11458
David Baumgold <david@davidbaumgold.com>
2019-09-09 15:24:39 +0200

    test(snapshots): use GraphQL snapshot serializer (#27)

--- __tests__/integration/schema/core.js
+++ __tests__/integration/schema/core.js
@@ -1,6 +1,17 @@
 const printSchemaOrdered = require("../../printSchemaOrdered");
 const { withPgClient } = require("../../helpers");
 const { createPostGraphileSchema } = require("postgraphile-core");
+const { isSchema } = require("graphql")
+
+const GraphQLSchemaSerializer = {
+  test(val) {
+    return isSchema(val);
+  },
+  serialize(schema) {
+    return printSchemaOrdered(schema);
+  },
+};
+expect.addSnapshotSerializer(GraphQLSchemaSerializer);
 
 exports.test = (schemas, options, setup) => () =>
   withPgClient(async client => {
@@ -12,5 +23,5 @@ exports.test = (schemas, options, setup) => () =>
       }
     }
     const schema = await createPostGraphileSchema(client, schemas, options);
-    expect(printSchemaOrdered(schema)).toMatchSnapshot();
+    expect(schema).toMatchSnapshot();
   });
