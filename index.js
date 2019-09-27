14e1e12fc3e7d625595311e04b111eb85b17e2b0
Matt Bretl <mpbretl@gmail.com>
2019-09-27 10:00:06 -0500

    chore(lint): prettier index.js

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ module.exports = function PgManyToManyPlugin(builder, options) {
     // Check dependencies
     if (!build.versions) {
       throw new Error(
-        `Plugin ${pkg.name}@${
-          pkg.version
-        } requires graphile-build@^4.1.0 in order to check dependencies (current version: ${
-          build.graphileBuildVersion
-        })`
+        `Plugin ${pkg.name}@${pkg.version} requires graphile-build@^4.1.0 in order to check dependencies (current version: ${build.graphileBuildVersion})`
       );
     }
     const depends = (name, range) => {
