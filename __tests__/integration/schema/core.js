15bd02c045b5d460bfbc7244ad3edde44246aef3
David Baumgold <david@davidbaumgold.com>
2020-02-13 08:42:31 +0100

    test: use jest-serializer-graphql-schema (#41)

--- __tests__/integration/schema/core.js
+++ __tests__/integration/schema/core.js
@@ -1,17 +1,5 @@
-const printSchemaOrdered = require("../../printSchemaOrdered");
 const { withPgClient } = require("../../helpers");
 const { createPostGraphileSchema } = require("postgraphile-core");
-const { isSchema } = require("graphql");
-
-const GraphQLSchemaSerializer = {
-  test(val) {
-    return isSchema(val);
-  },
-  serialize(schema) {
-    return printSchemaOrdered(schema);
-  },
-};
-expect.addSnapshotSerializer(GraphQLSchemaSerializer);
 
 exports.test = (schemas, options, setup) => () =>
   withPgClient(async client => {
