cef3dabf5d55c9bc97a6aaca76bbde0b95f30850
Benjie Gillam <benjie@jemjie.com>
2022-11-23 15:13:59 +0000

    Enable TypeScript

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const PgManyToManyPlugin: import("graphile-build").Plugin;
-export default PgManyToManyPlugin;
--- src/PgManyToManyRelationEdgeColumnsPlugin.ts
+++ src/PgManyToManyRelationEdgeColumnsPlugin.ts
@@ -1,3 +1,4 @@
+/** @type {GraphileConfig.Plugin } */
 const PgManyToManyRelationEdgeColumnsPlugin = {
   name: "PgManyToManyRelationEdgeColumnsPlugin",
 
