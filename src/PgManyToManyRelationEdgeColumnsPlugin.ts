3c957091699a197cf843ebf839e37ab8661152b5
Benjie Gillam <benjie@jemjie.com>
2022-11-24 11:14:36 +0000

    More type fixes

--- src/PgManyToManyRelationEdgeColumnsPlugin.ts
+++ src/PgManyToManyRelationEdgeColumnsPlugin.ts
@@ -38,13 +38,17 @@ export const PgManyToManyRelationEdgeColumnsPlugin: GraphileConfig.Plugin = {
         }
 
         const {
-          leftKeyAttributes,
+          leftTable,
+          leftRelationName,
           junctionTable,
-          junctionLeftKeyAttributes,
-          junctionRightKeyAttributes,
-          rightKeyAttributes,
+          rightRelationName,
           allowsMultipleEdgesToNode,
         } = pgManyToManyRelationship;
+
+        const junctionLeftKeyAttributeNames =
+          leftTable.getRelation(leftRelationName).remoteColumns;
+        const junctionRightKeyAttributeNames =
+          junctionTable.getRelation(rightRelationName).localColumns;
 
         if (allowsMultipleEdgesToNode) {
           return fields;
@@ -52,7 +56,7 @@ export const PgManyToManyRelationEdgeColumnsPlugin: GraphileConfig.Plugin = {
 
         return extend(
           fields,
-          Object.entries(junctionTable.columns as PgTypeColumns).reduce(
+          Object.entries(junctionTable.codec.columns).reduce(
             (memo, [columnName, column]) => {
               const behavior = build.pgGetBehavior(column.extensions);
               if (!build.behavior.matches(behavior, "select", "select")) {
@@ -60,17 +64,9 @@ export const PgManyToManyRelationEdgeColumnsPlugin: GraphileConfig.Plugin = {
               }
 
               // Skip left and right key attributes
-              if (
-                junctionLeftKeyAttributes
-                  .map((a) => a.columnName)
-                  .includes(columnName)
-              )
+              if (junctionLeftKeyAttributeNames.includes(columnName))
                 return memo;
-              if (
-                junctionRightKeyAttributes
-                  .map((a) => a.columnName)
-                  .includes(columnName)
-              )
+              if (junctionRightKeyAttributeNames.includes(columnName))
                 return memo;
 
               const fieldName = inflection.column({
@@ -95,62 +91,18 @@ export const PgManyToManyRelationEdgeColumnsPlugin: GraphileConfig.Plugin = {
                       isPgManyToManyRelationEdgeColumnField: true,
                       pgFieldCodec: column.codec,
                     },
-                    () => {
-                      /*
-                      // Since we're ignoring multi-column keys, we can simplify here
-                      const leftKeyAttribute = leftKeyAttributes[0];
-                      const junctionLeftKeyAttribute =
-                        junctionLeftKeyAttributes[0];
-                      const junctionRightKeyAttribute =
-                        junctionRightKeyAttributes[0];
-                      const rightKeyAttribute = rightKeyAttributes[0];
-
-                      const sqlSelectFrom = sql.fragment`select ${sql.identifier(
-                        columName
-                      )} from ${sql.identifier(
-                        junctionTable.namespace.name,
-                        junctionTable.name
-                      )}`;
-
-                      addDataGenerator((parsedResolveInfoFragment) => {
-                        return {
-                          pgQuery: (queryBuilder) => {
-                            queryBuilder.select(
-                              getSelectValueForFieldAndTypeAndModifier(
-                                ReturnType,
-                                fieldContext,
-                                parsedResolveInfoFragment,
-                                sql.fragment`(${sqlSelectFrom} where ${sql.identifier(
-                                  junctionRightKeyAttribute.name
-                                )} = ${queryBuilder.getTableAlias()}.${sql.identifier(
-                                  rightKeyAttribute.name
-                                )} and ${sql.identifier(
-                                  junctionLeftKeyAttribute.name
-                                )} = ${queryBuilder.parentQueryBuilder.parentQueryBuilder.getTableAlias()}.${sql.identifier(
-                                  leftKeyAttribute.name
-                                )})`,
-                                type,
-                                typeModifier
-                              ),
-                              fieldName
-                            );
-                          },
-                        };
-                      });
-                      */
-                      return {
-                        description: column.description,
-                        type: nullableIf(
-                          !column.notNull &&
-                            !column.codec.notNull &&
-                            !column.extensions?.tags?.notNull,
-                          ReturnType
-                        ),
-                        plan($edge: PgSelectSingleStep<any, any, any, any>) {
-                          return $edge.get(columnName);
-                        },
-                      };
-                    }
+                    () => ({
+                      description: column.description,
+                      type: nullableIf(
+                        !column.notNull &&
+                          !column.codec.notNull &&
+                          !column.extensions?.tags?.notNull,
+                        ReturnType
+                      ),
+                      plan($edge: PgSelectSingleStep<any, any, any, any>) {
+                        return $edge.get(columnName);
+                      },
+                    })
                   ),
                 },
                 `PgManyToMany adding edge field for '${columnName}'.`
